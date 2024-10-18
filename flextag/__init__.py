ab7f93b1a3638f4bbcdae90118dde305370257e2
DarrenHaba <darrenhaba@live.com>
2024-10-17 22:25:06 -0500

    Fix linting errors

--- flextag/__init__.py
+++ flextag/__init__.py
@@ -1,1 +1,12 @@
-from .flextag import flex_to_dict, dict_to_flex  # noqa: F401
+"""
+This is the __init__.py file for the flextag package.
+
+It imports core functions for parsing and generating Flex Tag format.
+"""
+
+from .flextag import (  # noqa: F401
+    flex_to_dict,
+    dict_to_flex,
+    flex_to_json,
+    json_to_flex,
+)
